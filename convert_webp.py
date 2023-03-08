3c7f1dbcc809879c4cfce2bb04f1db7e9e2dc329
yuna04 <lake651@gmail.com>
2023-03-08 14:19:39 +0100

    Added option to convert all webps in the current working directory.

--- convert_webp.py
+++ convert_webp.py
@@ -1,5 +1,19 @@
 from PIL import Image, ImageSequence
-import sys
+import sys, os
+
+def convert_png(img, s):
+    img.save(f"{s[0]}.png", "PNG")
+
+def convert_gif(img, s):
+    img.info.pop('background', None)
+    img.save(f"{s[0]}.gif", "GIF", save_all=True)
+
+def checkIfAnimated(img, i):
+    for frame in ImageSequence.Iterator(img):
+        print(i)
+        i += 1
+        if i >= 2:
+            return i
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -8,19 +22,32 @@ if __name__ == "__main__":
         print("You've provided too many arguments.\nOnly provide the file you wish to convert.")
     else:
         try:
-            img = Image.open(sys.argv[1])
+            if sys.argv[1] == "ALL":
+                cwd = os.getcwd()
+                file_list = os.listdir()
+                for file in file_list:
+                    print(file)
+                    if file.split(".")[-1] == "webp":
+                        img = Image.open(file)
+                        s = file.split(".")
+                        i = 0
+                        i = checkIfAnimated(img, i)
+                        print(i)
+                        if i > 1:
+                            convert_gif(img, s)
+                        else:
+                            convert_png(img, s)
+
+            img = Image.open(sys.argv[1]) 
             s = sys.argv[-1].split(".")
             i = 0
-            for frame in ImageSequence.Iterator(img):
-                i += 1
-                if i >= 2:
-                    break                
+            i = checkIfAnimated(img, i)
             if s[-1] != "webp":
                 print("File is not webp and will not be converted.")
             elif i > 1:
-                img.info.pop('background', None)
-                img.save(f"{s[0]}.gif", "GIF", save_all=True)
+                convert_gif(img, s)
             else:
-                img.save(f"{s[0]}.png", "PNG")
+                convert_png(img, s)
+                
         except OSError:
             print(f"Cannot convert {sys.argv[1]}")
